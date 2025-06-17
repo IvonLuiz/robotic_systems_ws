e13baac2893d799f79216dfa6fde1fcac7e4d901
Carlos Cerqueira <ce.cesc01@gmail.com>
2025-06-17 19:07:57 -0300

    feat: add initial implementation

--- src/ur5_controller/include/ur5_controller/ur5_controller.hpp
+++ src/ur5_controller/include/ur5_controller/ur5_controller.hpp
@@ -1,13 +1,41 @@
 #pragma once
 
+#include "ur5_interface/action/move_to_pose.hpp"
+#include <functional>
 #include <memory>
 #include <moveit/robot_model/robot_model.hpp>
 #include <moveit/robot_model_loader/robot_model_loader.hpp>
 #include <moveit/robot_state/robot_state.hpp>
+#include <rclcpp/logging.hpp>
 #include <rclcpp/rclcpp.hpp>
 #include <rclcpp/utilities.hpp>
+#include <rclcpp_action/create_server.hpp>
+#include <rclcpp_action/rclcpp_action.hpp>
+#include <rclcpp_action/server.hpp>
+#include <rclcpp_action/server_goal_handle.hpp>
+#include <rclcpp_action/types.hpp>
+#include <string>
+
+static const std::string PLANNING_GROUP = "ur_manipulator";
 
 class UR5Controller : public rclcpp::Node {
+private:
+  rclcpp_action::Server<ur5_interface::action::MoveToPose>::SharedPtr _server;
+
+  rclcpp_action::GoalResponse pose_callback(
+      const rclcpp_action::GoalUUID &uuid,
+      std::shared_ptr<const ur5_interface::action::MoveToPose::Goal> goal);
+
+  rclcpp_action::CancelResponse pose_cancel(
+      const std::shared_ptr<
+          rclcpp_action::ServerGoalHandle<ur5_interface::action::MoveToPose>>
+          goal_handle);
+
+  void handle_pose_callback(
+      const std::shared_ptr<
+          rclcpp_action::ServerGoalHandle<ur5_interface::action::MoveToPose>>
+          goal_handle);
+
 public:
   UR5Controller();
 };
--- src/ur5_controller/src/ur5_controller.cpp
+++ src/ur5_controller/src/ur5_controller.cpp
@@ -1,6 +1,48 @@
 #include "ur5_controller/ur5_controller.hpp"
 
-UR5Controller::UR5Controller() : rclcpp::Node("UR5_CONTROLLER") {}
+rclcpp_action::GoalResponse UR5Controller::pose_callback(
+    const rclcpp_action::GoalUUID &uuid,
+    std::shared_ptr<const ur5_interface::action::MoveToPose::Goal> goal) {
+  // TODO: ADD THIS IMPLEMENTATION
+  RCLCPP_INFO(this->get_logger(), "Received goal request");
+  (void)uuid;
+  (void)goal;
+  return rclcpp_action::GoalResponse::ACCEPT_AND_EXECUTE;
+}
+
+rclcpp_action::CancelResponse UR5Controller::pose_cancel(
+    const std::shared_ptr<
+        rclcpp_action::ServerGoalHandle<ur5_interface::action::MoveToPose>>
+        goal_handle) {
+  // TODO: ADD THIS IMPLEMENTATION
+  RCLCPP_INFO(this->get_logger(), "Received request to cancel goal");
+  (void)goal_handle;
+  return rclcpp_action::CancelResponse::ACCEPT;
+}
+
+void UR5Controller::handle_pose_callback(
+    const std::shared_ptr<
+        rclcpp_action::ServerGoalHandle<ur5_interface::action::MoveToPose>>
+        goal_handle) {
+  // TODO: ADD THIS IMPLEMENTATION
+  RCLCPP_INFO(this->get_logger(), "Executing goal...");
+  (void)goal_handle;
+}
+
+UR5Controller::UR5Controller() : rclcpp::Node("UR5_CONTROLLER") {
+  this->declare_parameter("pose_action_server_name", "pose_server");
+
+  RCLCPP_INFO(this->get_logger(), "Iniciando UR5Controller Node...");
+
+  this->_server =
+      rclcpp_action::create_server<ur5_interface::action::MoveToPose>(
+          this, this->get_parameter("pose_action_server_name").as_string(),
+          std::bind(&UR5Controller::pose_callback, this, std::placeholders::_1,
+                    std::placeholders::_2),
+          std::bind(&UR5Controller::pose_cancel, this, std::placeholders::_1),
+          std::bind(&UR5Controller::handle_pose_callback, this,
+                    std::placeholders::_1));
+}
 
 int main(int argc, char *argv[]) {
   rclcpp::init(argc, argv);
