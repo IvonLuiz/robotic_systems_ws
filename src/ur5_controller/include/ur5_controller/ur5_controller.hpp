c0eeea56a2e650513d7ff1951cc64f8c0eb43313
Carlos Cerqueira <ce.cesc01@gmail.com>
2025-06-17 17:51:44 -0300

    feat: create basic node

--- src/ur5_controller/include/ur5_controller/ur5_controller.hpp
+++ src/ur5_controller/include/ur5_controller/ur5_controller.hpp
@@ -1,7 +1,13 @@
 #pragma once
 
+#include <memory>
+#include <moveit/robot_model/robot_model.hpp>
+#include <moveit/robot_model_loader/robot_model_loader.hpp>
+#include <moveit/robot_state/robot_state.hpp>
 #include <rclcpp/rclcpp.hpp>
+#include <rclcpp/utilities.hpp>
 
-class UR5Controller : public rclcpp::Node
-{
+class UR5Controller : public rclcpp::Node {
+public:
+  UR5Controller();
 };
--- src/ur5_controller/src/ur5_controller.cpp
+++ src/ur5_controller/src/ur5_controller.cpp
@@ -1,1 +1,12 @@
-int main() {}
+#include "ur5_controller/ur5_controller.hpp"
+
+UR5Controller::UR5Controller() : rclcpp::Node("UR5_CONTROLLER") {}
+
+int main(int argc, char *argv[]) {
+  rclcpp::init(argc, argv);
+
+  rclcpp::spin(std::make_shared<UR5Controller>());
+
+  rclcpp::shutdown();
+  return 0;
+}
